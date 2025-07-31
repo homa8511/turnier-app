9041cda8e209d9fe2505de82c110d60b90d0cace
Markus <markus@MacBook-Pro-von-Markus.local>
2025-07-31 14:22:03 +0200

    fix: execute api tests in runner

--- backend/src/infrastructure/database.ts
+++ backend/src/infrastructure/database.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: 'db',
+  host: process.env.POSTGRES_HOST || 'db', // Nutzt localhost im Test, sonst 'db'
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
