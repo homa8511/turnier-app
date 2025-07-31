a972f1089af74cf8291584d01650143897114995
Markus <markus@MacBook-Pro-von-Markus.local>
2025-07-31 12:17:38 +0200

    fix: adapt eslint rules for backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -8,5 +8,12 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    // Erlaube unbenutzte Variablen, die mit einem Unterstrich beginnen
+    // Dies ist nützlich für Funktionssignaturen wie (req, res, next)
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { 'argsIgnorePattern': '^_' }
+    ]
+  },
 };
